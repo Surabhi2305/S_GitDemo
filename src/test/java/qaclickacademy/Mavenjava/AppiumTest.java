1533f3d77175b18cf3042d221ca3ca697bd160cd
Surabhi <=>
2022-05-02 10:57:07 +0530

    This is my first Commit

---
+++ src/test/java/qaclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void NativeAndroidApp() {
+		System.out.println("NativeAndroid test is done");
+		
+	}
+	@Test
+	public void IOSApp() {
+		System.out.println("IOS test is done");
+		
+	}
+
+}
---
+++ src/test/java/qaclickacademy/Mavenjava/RestApiTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy.Mavenjava;
+
+
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+	
+	@Test
+	public void PostJeera() {
+		System.out.println("Defect tracking tool jeeta");
+	}
+	
+	@Test
+	public void DeleteTwitter() {
+		System.out.println("Twitter account is deleted");	
+	}
+
+}
